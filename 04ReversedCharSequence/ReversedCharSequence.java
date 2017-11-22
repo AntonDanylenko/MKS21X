6b25b1696c2afff873f3edd2665d2eee6943fbd2
AntonDanylenko <adanylenko@stuy.edu>
2017-11-22 14:42:19 -0500

    first

---
+++ 04ReversedCharSequence/ReversedCharSequence.java
@@ -0,0 +1,7 @@
+public class ReversedCharSequence implements CharSequence{
+    String data;
+    
+    public ReversedCharSequence(String given){
+	for (int n=0; n<given.length(); n+=){
+	    
+	    

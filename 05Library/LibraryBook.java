47d502e7296c16e8e003a0ceb11bec5f0b50cf27
AntonDanylenko <adanylenko@stuy.edu>
2017-11-28 14:37:04 -0500

    constructor

---
+++ 05Library/LibraryBook.java
@@ -0,0 +1,8 @@
+public abstract class LibraryBook extends Book implements Comparable<LibraryBook>{
+    private String callNumber;
+
+    public LibraryBook(String author, String title, String ISBN, String callNumber){
+	super(author, title, ISBN);
+	this.callNumber = callNumber;
+    }
+}

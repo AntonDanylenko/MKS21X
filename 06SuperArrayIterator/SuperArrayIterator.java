f2c2d81f9b535c32ec32a75288480a203dcd3ccf
Anton Danylenko <adanylenko@stuy.edu>
2017-12-05 20:21:24 -0500

    added next() hasNext() and remove()

--- 06SuperArrayIterator/SuperArrayIterator.java
+++ 06SuperArrayIterator/SuperArrayIterator.java
@@ -1,8 +1,29 @@
 import java.util.Iterator;
 
-public class SuperArrayIterator{
+public class SuperArrayIterator implements Iterator<String>{
 	SuperArray SA;
-	
+	int index;
+
 	public SuperArrayIterator(SuperArray SA, int index){
-		
+		this.SA = SA;
+		this.index = index;
+	}
+
+	public String next(){
+		if (hasNext()){
+			index++;
+		}
+		else{
+			System.exit(0);
+		}
+		return index-1;
+	}
+
+	public boolean hasNext(){
+		return index<SA.size();
+	}
+
+	public void remove(){
+		throw new UnsupportedOperationException();
+    }
 }

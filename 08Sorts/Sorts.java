b1d61af9eeaceea9496796f8b70f5d6870dd8301
AntonDanylenko <adanylenko@stuy.edu>
2017-12-07 14:44:15 -0500

    wrote main to test

--- 08Sorts/Sorts.java
+++ 08Sorts/Sorts.java
@@ -1,6 +1,6 @@
 public class Sorts{
 	public static String name(){
-		return "01.Danylenko.Anton"; 
+		return "09.Danylenko.Anton"; 
 	}
 
 	public static int findSmlst(int[] data, int start){
@@ -13,7 +13,7 @@ public class Sorts{
 		return posSmlst;
 	}
 
-	public static void replace(int[] data; int value1; int value2){
+    public static void swap(int[] data, int value1, int value2){
 		int temp = data[value1];
 		data[value1] = data[value2];
 		data[value2] = temp;
@@ -21,7 +21,29 @@ public class Sorts{
 
 	public static void selectionSort(int[] data){
 		for (int n=0; n<data.length; n++){
-			replace(data, data[n], findSmlst(data, n));
+			swap(data, n, findSmlst(data, n));
 		}
 	}
+
+
+    
+    public static void main(String[]args){
+	int[] randish = new int[15];
+	for (int i=0; i<randish.length; i++){
+	    randish[i]=(int)(Math.random()*100);
+	}
+	String toPrint = "";
+	for (int r=0; r<randish.length; r++){
+	    toPrint +=  randish[r];
+	    toPrint += ", ";
+	}
+	System.out.println("Unsorted: " + toPrint);
+        selectionSort(randish);
+	String toPrint2 = "";
+	for (int r=0; r<randish.length; r++){
+	    toPrint2 += randish[r];
+	    toPrint2 += ", ";
+	}
+	System.out.println("Sorted: " + toPrint2);
+    }
 }

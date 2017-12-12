5e70048ca6e0cb6f30b6ca1c0a75bdb262b152b6
Anton Danylenko <adanylenko@stuy.edu>
2017-12-12 17:41:36 -0500

    basic structure with name(), CtoF(), and FtoC()

---
+++ Temperature.java
@@ -0,0 +1,16 @@
+public class Temperature{
+	//Naming
+	public static String name(){
+		return "09.Danylenko.Anton"; 
+	}
+
+
+	//Celcius to Fahrenheit
+	public static double CtoF(double t){
+	}
+
+
+	//Fahrenheit to Celcius
+	public static double FtoC(double t){
+	}
+}
